1117379d0167f2e5e6e847ec0315f8c1c20ed70f
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-10-22 07:55:40 +0600

    fireabase auth and config added

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Fahim",
+  description: "A Next 13 Fahim  Muntashir Portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return <ClerkProvider></ClerkProvider>;
+}
---
+++ firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzKlsfdQJa2rkvLZTca1e5GBY5BLPVq44",
+  authDomain: "fahim-muntashir.firebaseapp.com",
+  projectId: "fahim-muntashir",
+  storageBucket: "fahim-muntashir.appspot.com",
+  messagingSenderId: "1077061353219",
+  appId: "1:1077061353219:web:f0d9601a7ab23d60c63336",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
